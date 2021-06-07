0e5f65d4b5e8e92cbe9e5ace02c4e45366d91121
Caroline Freitas <54582414+ccarol-freitas@users.noreply.github.com>
2021-06-07 17:50:29 -0300

    insert naves

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const navesCont = document.getElementById("naves");
 preencheContador();
 preencherTabela();
 preencherDadosDoPersonagem();
+preencherTabelaPlanetas();
+preencherDadosDaNave();
 
 google.charts.load("current", { packages: ["corechart"] });
 google.charts.setOnLoadCallback(drawChart);
@@ -25,7 +27,8 @@ async function drawChart() {
   var options = {
     title: "Veículos de Star Wars",
     // legend: "none",
-    is3D: true
+    is3D: true,
+    colors: ["#b893e8", "#007aff", "yellow", "green", "red"]
   };
 
   var chart = new google.visualization.PieChart(
@@ -70,7 +73,6 @@ function swapGet(param) {
 async function preencherDadosDoPersonagem() {
   const response = await swapGet("people/");
   const cardPerson = response.data.results;
-  console.log("AQUI" + cardPerson);
 
   cardPerson.forEach((people) => {
     $("#people").append(`<div class="card-content-people">
@@ -78,10 +80,119 @@ async function preencherDadosDoPersonagem() {
           <div class="card-info">
           <div class="star-map"><img src="./assets/mapa_estrelar.jpg"></div>
             <h4> ${people.name}</h4>
-            <p class="subtitle-people">Gênero: ${people.gender}</p>
-            <p class="subtitle-people">Ano de Nascimento: ${people.birth_year}</p>
+            <p class="subtitle-people"><b>Gênero:</b> ${people.gender}</p>
+            <p class="subtitle-people"><b>Ano de Nascimento:</b> ${people.birth_year}</p>
           </div>
         </div>
       </div>`);
   });
 }
+
+async function preencherTabelaPlanetas() {
+  const response = await swapGet("planets/");
+  const table = response.data.results;
+  console.log(table);
+  table.forEach((planet) => {
+    $("#planets").append(`<tr>
+    <td>${planet.name}</td>
+    <td>${planet.climate}</td>
+    <td>${planet.terrain}</td>
+    <td>${planet.gravity}</td>
+    <td>${planet.population}</td>
+    </tr>`);
+  });
+}
+
+async function preencherDadosDaNave() {
+  const response = await swapGet("starships/");
+  const cardStarship = response.data.results;
+
+  cardStarship.forEach((starship) => {
+    $("#starship").append(`<div class="card-content-starship">
+        <div class="card-starship">
+          <div class="card-info">
+          <div class="star-map"><img src="./assets/mapa_estrelar.jpg"></div>
+            <h4> ${starship.name}</h4>
+            <p class="subtitle-starship"><b>Classe:</b> ${starship.starship_class}</p>
+            <p class="subtitle-starship"><b>Modelo:</b> ${starship.model}</p>
+          </div>
+        </div>
+      </div>`);
+  });
+}
+document.addEventListener("DOMContentLoaded", function (event) {
+  let nave = document.querySelectorAll(".corcinza strong");
+
+  let nave1 = document.createElement("img");
+  nave1.src = "";
+
+  let nave2 = document.createElement("img");
+  nav2.src = "";
+
+  let nave3 = document.createElement("img");
+  nave3.src = "";
+
+  let nave4 = document.createElement("img");
+  nave4.src =
+    "http://colombo--tst.custhelp.com/euf/assets/images/icon_colombo/servicos.png";
+
+  let nave5 = document.createElement("img");
+  nave5.src = "";
+
+  let nave6 = document.createElement("img");
+  nave6.src = "";
+
+  let nave7 = document.createElement("img");
+  nave7.src = "";
+
+  let nave8 = document.createElement("img");
+  nave8.src = "";
+
+  let nave9 = document.createElement("img");
+  nave9.src = "";
+
+  let nave10 = document.createElement("img");
+  nave10.src = "";
+
+  for (let i = 0; i < cat.length; i++) {
+    if (cat[i].innerText == "Entregas") {
+      cat[i].appendChild(nave1);
+    }
+
+    if (cat[i].innerText == "Loja Física") {
+      cat[i].appendChild(nave2);
+    }
+
+    if (cat[i].innerText == "Informações Gerais") {
+      cat[i].appendChild(nave3);
+    }
+
+    if (cat[i].innerText == "Serviços Financeiros") {
+      cat[i].appendChild(nave4);
+    }
+
+    if (cat[i].innerText == "Garantia") {
+      cat[i].appendChild(nave5);
+    }
+
+    if (cat[i].innerText == "Trocas e Cancelamentos") {
+      cat[i].appendChild(nave6);
+    }
+
+    if (cat[i].innerText == "Trocas e Cancelamentos") {
+      cat[i].appendChild(nave7);
+    }
+
+    if (cat[i].innerText == "Trocas e Cancelamentos") {
+      cat[i].appendChild(nave8);
+    }
+
+    if (cat[i].innerText == "Trocas e Cancelamentos") {
+      cat[i].appendChild(nave9);
+    }
+
+    if (cat[i].innerText == "Trocas e Cancelamentos") {
+      cat[i].appendChild(nave10);
+    }
+  }
+});
